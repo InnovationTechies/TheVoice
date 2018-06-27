9e6bf661c3c2ed149061c68709cc62af7b4823a6
Cwenga Zozo <cwengazozo@outlook.com>
2018-06-27 19:01:10 +0200

    Final Wednesday

--- TheVoice/MainActivity.cs
+++ TheVoice/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Media;
+
 
 
 
@@ -9,13 +11,21 @@ namespace TheVoice
     [Activity(Label = "TheVoice", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        MediaPlayer _player;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-        }
+
+            _player = MediaPlayer.Create(this, Resource.Raw.test);
+            var playButton = FindViewById<Button>(Resource.Id.playButton);
+            playButton.Click += delegate {
+                _player.Start();
+            }
 
         protected void SaveVoice()
         {
